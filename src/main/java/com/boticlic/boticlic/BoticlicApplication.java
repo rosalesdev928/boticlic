3ef4202c27fc8db8445b276d2bf0673c36b35584
Leonardo Rosales <rosalesdev928@gmail.com>
2026-05-17 17:09:30 -0500

    primer commit - proyecto boticlic

---
+++ src/main/java/com/boticlic/boticlic/BoticlicApplication.java
@@ -0,0 +1,13 @@
+package com.boticlic.boticlic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoticlicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoticlicApplication.class, args);
+    }
+
+}
